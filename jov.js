f604607a92d76e35099d82c3456237e99ec7e2ed
Abdulrehman-8 <abdulrehmana3478@gmail.com>
2024-06-15 16:01:33 +0500

    Added the number randomizer. Checkpoint reached!

--- jov.js
+++ jov.js
@@ -1,3 +1,4 @@
 //Attemping to add reactive number.
+let randumNum = Math.floor(Math.random() * (24 - 5) + 5) /* So here, we use this to get a random numbre.. kind of saw this online but works. */
 let joem = document.getElementById('testing');
-joem.textContent = "nicer"
+joem.textContent = `Your number is: ${randumNum}`;
