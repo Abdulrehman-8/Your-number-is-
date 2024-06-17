14aea5d412685f3af91acc1a392b1cc0b9ef4ade
Abdulrehman-8 <abdulrehmana3478@gmail.com>
2024-06-18 01:03:37 +0500

    Sucessfully captured the answer of the addition, and prevented the answer from spawning more than once!

--- jov.js
+++ jov.js
@@ -1,4 +1,3 @@
-//Attemping to add reactive number.
 let obj1 = [
 document.getElementById("box1"),
 document.getElementById("box2"),
@@ -9,11 +8,62 @@ document.getElementById("box6"),
 document.getElementById("box7"),
 document.getElementById("box8"),
 document.getElementById("box9"),
-]
-let randumNum = Math.floor(Math.random() * (26 - 12) + 12) /* So here, we use this to get a random numbre.. kind of saw this online but works. */
+];
+let randomerNum = Math.floor(Math.random() * (22 - 3) + 3)
+let randumNum = Math.floor(Math.random() * (22 - 3) + 3) /* So here, we use this to get a random numbre.. kind of saw this online but works. */
 let joem = document.getElementById('testing');
-joem.textContent = `Your number is: ${randumNum} + ${Math.floor(Math.random() * (26 - 12) + 12)}`;
+joem.textContent = `Your number is: ${randumNum} + ${randomerNum}`
 for (objs of obj1) {
-    objs.textContent = `${Math.floor(Math.random() * (11 - 2) + 2)}`
+    objs.textContent = `${Math.floor(Math.random() * (43 - 5) + 5)}`
 }
-//create array maybe to organize this code (and practice)?
+let catcher = randomerNum + randumNum;
+let int = Math.floor(Math.random() * (9 - 1) + 1)
+if (obj1[0].textContent === catcher) {
+    obj1[0].textContent = 3
+} else if (obj1[1].textContent === catcher) {
+    obj1[1].textContent = 4
+}else if (obj1[2].textContent === catcher) {
+    obj1[2].textContent = 5
+}else if (obj1[3].textContent === catcher) {
+    obj1[3].textContent = 3
+}else if (obj1[4].textContent === catcher) {
+    obj1[4].textContent = 4
+}else if (obj1[5].textContent === catcher) {
+    obj1[5].textContent = 5
+}else if (obj1[6].textContent === catcher) {
+    obj1[6].textContent = 3
+}else if (obj1[7].textContent === catcher) {
+    obj1[7].textContent = 4
+}else if (obj1[8].textContent === catcher) {
+    obj1[8].textContent = 5
+}
+switch (int) {
+    case 1:
+        obj1[0].textContent = catcher;
+        break;
+     case 2:
+         obj1[1].textContent = catcher;
+         break;
+      case 3:
+          obj1[2].textContent = catcher;
+          break;
+       case 4:
+            obj1[3].textContent = catcher;
+            break;
+        case 5:
+             obj1[4].textContent = catcher;
+             break;
+         case 6:
+              obj1[5].textContent = catcher;
+              break;
+          case 7:
+               obj1[6].textContent = catcher;
+               break;
+            case 8:
+                obj1[7].textContent = catcher;
+                break;
+             case 9:
+                 obj1[8].textContent = catcher;
+                 break;    
+}
+console.log(int);
